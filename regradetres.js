0fb0814e3cec169247cdd18d78398014761640b1
Artur Silva <arturdasilvadossantos.2021@gmail.com>
2025-09-20 20:16:35 -0300

    Add files via upload

---
+++ regradetres.js
@@ -0,0 +1,13 @@
+//Pegando inputs:
+const inputA = document.querySelector("#inputA");
+const inputB = document.querySelector("#inputB");
+const inputC = document.querySelector("#inputC");
+const divIncognita = document.querySelector("#incognita")
+const botaoEnviar = document.querySelector("input[type=button]");
+let resultado = "";
+
+//Adicionando evento quando o botão for clicado e executando a função com arrow function:
+botaoEnviar.addEventListener("click", () => {
+    resultado = (Number(inputB.value) * Number(inputC.value)) / Number(inputA.value)
+    divIncognita.textContent = resultado
+});
